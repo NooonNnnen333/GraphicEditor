566a011429c3768061fd57d3b0ebfc087c616fbe
NooonNnnen333 <egorvas05@gmail.com>
2025-05-05 00:54:28 +0500

    Обновил MAUI-файлы из GraphicEditor

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -16,5 +16,7 @@ public partial class MainPage : ContentPage
         BindingContext = _viewModel;
     }
     
+    
+    
 }
     
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
+using ColorPicker;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace GraphicApp;
 
@@ -11,6 +13,7 @@ public static class MauiProgram
         var builder = MauiApp.CreateBuilder();
         builder
             .UseMauiApp<App>()
+            .UseSkiaSharp()
             // Initialize the .NET MAUI Community Toolkit by adding the below line of code
             .UseMauiCommunityToolkit()
             // After initializing the .NET MAUI Community Toolkit, optionally add additional fonts
