ca791589b71c74bd5a25f52a364a95063a65c999
NooonNnnen333 <egorvas05@gmail.com>
2025-05-04 15:01:44 +0500

    Предыдущая версия

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace L1;
+
+public partial class MainPage : ContentPage
+{
+    
+    public ViewModel _viewModel;
+    
+    public MainPage()
+    {
+        InitializeComponent();
+
+        // Создаём ViewModel и передаём ей ссылку на CanvasControl из XAML
+        _viewModel = new ViewModel(CanvasControl);
+
+        // Назначаем ViewModel в качестве BindingContext (если нужно биндинги)
+        BindingContext = _viewModel;
+    }
+
+
+}
