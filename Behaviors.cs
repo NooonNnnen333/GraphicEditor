0119d6a07353b4a6f302757a96208c83e5551d0d
NooonNnnen333 <egorvas05@gmail.com>
2025-05-04 17:58:13 +0500

    Добавил перемещение по холсту

---
+++ Behaviors.cs
@@ -0,0 +1,32 @@
+namespace GraphicApp;
+
+public class BehaviorsClass : Behavior<ScrollView>
+{
+    
+    double _startX, _startY;
+
+    protected override void OnAttachedTo(ScrollView scroll)
+    {
+        base.OnAttachedTo(scroll);
+
+        var pan = new PanGestureRecognizer();
+        pan.PanUpdated += (s, e) =>
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Started:
+                    _startX = scroll.ScrollX;
+                    _startY = scroll.ScrollY;
+                    break;
+                case GestureStatus.Running:
+                    scroll.ScrollToAsync(
+                        _startX - e.TotalX,
+                        _startY - e.TotalY,
+                        false);
+                    break;
+            }
+        };
+
+        scroll.GestureRecognizers.Add(pan);
+    }
+}
